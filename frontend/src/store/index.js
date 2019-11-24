1b1830a53facd1b23883a5cc816874942f4a27ab
rg3915 <rg3915@yahoo.com.br>
2019-11-24 14:22:26 -0300

    lint

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -7,51 +7,56 @@ Vue.use(Vuex);
 const endpoint = 'http://localhost:8000';
 
 export default new Vuex.Store({
-    state: {
-        products: [],
+  state: {
+    products: [],
+  },
+  mutations: {
+    SET_PRODUCTS(state, obj) {
+      state.products = obj;
     },
-    mutations: {
-        SET_PRODUCTS(state, obj) {
-            state.products = obj;
-        },
-        ADD_PRODUCT(state, obj) {
-            state.products = [obj, ...state.products];
-        },
-        EDIT_PRODUCT(state, obj) {
-            const products = [...state.products]
-            const index = products.findIndex(p => p.pk == obj.pk);
-            products[index] = { ...obj };
-            state.products = products;
-        },
-        DELETE_PRODUCT(state, obj) {
-            state.products = state.products.filter(p => p.pk !== obj.pk);
-        }
+    ADD_PRODUCT(state, obj) {
+      state.products = [obj, ...state.products];
     },
-    actions: {
-        loadProducts(options) {
-            axios.get(endpoint + '/product/')
-            .then(response => {
-                options.commit('SET_PRODUCTS', response.data.data);
-            })
-        },
-        addProduct(options, obj) {
-          let bodyFormData = new FormData()
-          bodyFormData.append('obj', JSON.stringify(obj))
-          axios.post(endpoint + '/product/add/', bodyFormData)
-          .then(response => {
-            options.commit('ADD_PRODUCT', response.data);
-          })
-        },
-        editProduct(options, obj) {
-            let bodyFormData = new FormData()
-            bodyFormData.append('obj', JSON.stringify(obj))
-            axios.post(endpoint + '/product/' + obj.pk + '/', bodyFormData)
-            .then(response => {
-              options.commit('EDIT_PRODUCT', response.data);
-            })
-        },
-        deletePdroduct(options, obj) {
-            options.commit('DELETE_PRODUCT', obj);
-        }
+    EDIT_PRODUCT(state, obj) {
+      const products = [...state.products]
+      const index = products.findIndex(p => p.pk == obj.pk);
+      products[index] = { ...obj };
+      state.products = products;
+    },
+    DELETE_PRODUCT(state, obj) {
+      state.products = state.products.filter(p => p.pk !== obj.pk);
     }
+  },
+  actions: {
+    loadProducts(options) {
+      axios.get(endpoint + '/product/')
+        .then(response => {
+          options.commit('SET_PRODUCTS', response.data.data);
+        })
+    },
+    addProduct(options, obj) {
+      let bodyFormData = new FormData()
+      bodyFormData.append('obj', JSON.stringify(obj))
+      axios.post(endpoint + '/product/add/', bodyFormData)
+        .then(response => {
+          options.commit('ADD_PRODUCT', response.data);
+        })
+    },
+    editProduct(options, obj) {
+      let bodyFormData = new FormData()
+      bodyFormData.append('obj', JSON.stringify(obj))
+      axios.post(endpoint + '/product/' + obj.pk + '/', bodyFormData)
+        .then(response => {
+          options.commit('EDIT_PRODUCT', response.data);
+        })
+    },
+    deletePdroduct(options, obj) {
+      let bodyFormData = new FormData()
+      bodyFormData.append('obj', JSON.stringify(obj))
+      axios.post(endpoint + '/product/' + obj.pk + '/delete/', bodyFormData)
+        .then(response => {
+          options.commit('DELETE_PRODUCT', response.data);
+        })
+    }
+  }
 });
