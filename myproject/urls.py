afa0f4f6c58b5a15b6d219fca48adb2902291e2f
rg3915 <rg3915@yahoo.com.br>
2019-11-26 10:46:45 -0300

    Redirect all urls to index.

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('', include('myproject.core.urls')),
     path('product/', include('myproject.product.urls')),
     path('admin/', admin.site.urls),
+    re_path('^.*$', TemplateView.as_view(template_name="index.html")),
 ]
